8dc66b23576a5767ac3eb86233e34ca757775533
Arnel P. Tagupa <arneltagupa1431@gmail.com>
2026-04-20 16:04:21 +0800

    fix the 500 error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,19 +15,14 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts - non-render-blocking -->
+        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
-        <link rel="preload" as="style" href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" onload="this.onload=null;this.rel='stylesheet'">
-        <noscript><link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /></noscript>
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+        <noscript><link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"></noscript>
 
         <!-- Preconnect to image CDNs for faster LCP -->
         <link rel="preconnect" href="https://q-xx.bstatic.com">
         <link rel="preconnect" href="https://pix8.agoda.net">
-
-        <!-- Preload LCP image if available -->
-        @if(!empty($page['props']['lcpImageUrl']))
-        <link rel="preload" as="image" href="{{ $page['props']['lcpImageUrl'] }}" fetchpriority="high">
-        @endif
 
         <!-- Scripts -->
         @routes
