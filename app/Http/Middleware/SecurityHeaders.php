c3385469392ac7a4b187c7eab5fd048e1ae3ecec
Arnel P. Tagupa <arneltagupa1431@gmail.com>
2026-04-20 14:43:37 +0800

    Content-Security-Policy

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -28,11 +28,12 @@ class SecurityHeaders
         $response->headers->set('Content-Security-Policy-Report-Only',
             "default-src 'self'; " .
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://*.cloudflareinsights.com https://challenges.cloudflare.com; " .
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; " .
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://unpkg.com; " .
             "img-src 'self' data: blob: https: http:; " .
-            "font-src 'self' https://fonts.gstatic.com; " .
+            "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net; " .
             "connect-src 'self' https://api.stripe.com https://*.cloudflareinsights.com https://*.tile.openstreetmap.org; " .
             "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://www.youtube.com https://www.youtube-nocookie.com; " .
+            "worker-src 'self' blob:; " .
             "object-src 'none'; " .
             "base-uri 'self';"
         );
